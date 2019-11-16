e601e6ed381033667323125caa92ec4fa210bff3
Dean Stanley-Hunt <30317335+DeanSH@users.noreply.github.com>
2019-11-17 02:11:56 +1300

    First Commit

---
+++ index.php
@@ -0,0 +1,258 @@
+<head>
+
+<!--
+Design by R4
+-->
+
+<title>R4 Productionz</title>
+<meta name="keywords" content="">
+<meta name="description" content="The Only Place To Get The Best!">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="shortcut icon" href="images/favicon.ico">
+
+</head>
+
+<body>
+
+<div id="wrapper">
+
+<div id="top">
+	
+	<div id="lefta">
+	
+	<div id="logo"><a href="index.php"><img src="images/logo.png" width="550" alt="Home"></a></div> <!-- end logo //-->
+
+		 
+	</div> <!-- end left //-->
+
+	<div id="right">
+		<form method="get" id="searchform" action="/">
+	<div>
+	  <input value="search site" class="search" onfocus="if(this.value = 'Search...') { this.value = 'Search is currently disabled.'; }" type="text">
+	  <input class="submit" value="" type="submit">
+<br>
+<br>
+<br>
+<br>
+	</div>
+
+		</form>
+			  		 
+    </div> <!-- end right //-->
+
+
+  </div> <!-- end top //-->
+	
+	<div id="rss"><a href="index.php"><span>R4 - Nothing Compares!! Thinking Outside The Box!!</span></a></div> <!-- end rss //-->
+	
+	<ul id="nav">
+			<li><a href="index.php" title="R4">Home</a>		    </li>
+			<li><a href="free.html" title="Free Software">Freewares</a></li>
+			<li><a href="private.html" title="Paid Software">Paidwares</a></li>
+			<li><a href="music.html" title="Get Music Made By R4 Himself">R4 Music</a></li>
+			<li><a href="donate.html" title="How To Get Access">Donate!!</a></li>
+			<li><a href="tos.html" title="Terms Of Service / Rules For Customers">Terms & Rules</a></li>
+  </ul> 
+
+<!-- end nav //-->
+		
+	<ul id="subnav">
+                        <li><a href="yjapan.html" title="Show Y! Japan ENglish Patch!">Y!Japan</a></li>
+			<li><a href="chat.html" title="Show Chat/Help Zone">Register</a></li>
+			<li><a href="chat.html" title="Show Chat/Help Zone">Login</a></li>
+			<li><a href="chat.html" title="Show Chat/Help Zone">Chat/Help</a></li>
+</ul> <!-- end subnav //-->
+		
+		<div id="banner"><a href="donate.html"><span>How To Donate & Access Private Prog's - Click Here!</span></a></div>
+                <!-- end banner //-->
+
+	<div id="righta">
+	
+
+<div id="atop">Y! Japan <span class="color">Messenger Pack! (New)</span></div>
+	<div id="acontent">Yahoo Chat Lives On (Yahoo Japan!)</div>
+
+	<div id="afoot">
+		<div id="leftc"><a href="http://r4.productionz.ws/progs/Yahoo!%20Japan%20Messenger%20+%20English%20Patch%20+%20ID%20Creator.zip" title="Download">Download</a></div>
+		<div id="rightc"><a href="yjapan.html" title="More Info">More Info</a></div>
+	  </div>
+
+<div id="atop">Its Getting Harder Everyday <span class="color">By R4</span></div>
+	<div id="acontent">
+<center><div><embed name='RAOCXplayer' 
+src='music/Its%20Getting%20Harder%20Everyday%20-%20R4.mp3' 
+type='application/x-mplayer2' width='200' height='60' 
+autostart='1' ShowControls='1' ShowStatusBar='0' loop='true' 
+EnableContextMenu='0' DisplaySize='0' 
+pluginspage='http://www.microsoft.com/Windows/Downloads/Contents/Products/MediaPlayer/'>
+</embed></div></center>
+</div>
+	<div id="afoot">
+		<div id="leftc"><a href="music/Its%20Getting%20Harder%20Everyday%20-%20R4.zip" title="Download">Download</a></div>
+		<div id="rightc"><a href="music.html" title="More Info">More Info</a></div>
+	  </div>
+
+<div id="atop">Featured Full <span class="color">R4 HipHop Album!!</span></div>
+	<div id="acontent"><img src="images/album.JPG" Width="200" alt="Music Is My Weapon Album By R4"></div>
+
+	<div id="afoot">
+		<div id="leftc"><a href="http://r4.productionz.ws/music/Music%20Is%20My%20Weapon%20-%20By%20R4%20(Under%20Ground%20Album).zip" title="Download">Download</a></div>
+		<div id="rightc"><a href="music.html" title="More Info">More Info</a></div>
+	  </div>
+	
+<div id="atop">Hackers Attackers <span class="color">By R4</span></div>
+	<div id="acontent"><center>Track For My Hacking Haters!</center></div>
+	<div id="afoot">
+		<div id="leftc"><a href="music/Hackers%20Attackers%20-%20By%20R4.zip" title="Download">Download</a></div>
+		<div id="rightc"><a href="music.html" title="More Info">More Info</a></div>
+	  </div>
+
+        <div class="btop">T.O.S <span class="color">Rules</span></div>
+
+	<div class="bcontent">
+		<ul>
+			<li><a href="actrules.html" title="Rules Of Activation">Activation Rules</a><br /></li>
+			<li><a href="proxifier.html" title="Anti-Proxifier">Anti-Proxifier</a><br /></li>
+                	<li><a href="agreement.html" title="Terms Of Service Agreement">Terms Of Service</a><br /></li>
+                </ul>	
+        </div>
+		<div class="bfoot"></div>
+    
+		<div class="btop">Featured <span class="color">Sites</span></div>
+	<div class="bcontent">
+
+		<ul>
+			<li><a href="http://www.xe.com/currencyconverter/?From=USD&To=NZD" title="Currency Calculator Website">xe.com - Currency Conversion Site!</a></li>
+			<li><a href="http://yippychat.com/" title="Yippy!Chat">yippychat.com - Yippy's Main Site!</a></li>
+			<li><a href="http://meetsome1.com/" title="Dating Site">meetsome1.com - Free Dating Site!</a></li>
+			<li><a href="http://youtube.com/" title="You Tube">youtube.com - Free Video Site!</a></li>
+			<li><a href="http://twitter.com/" title="Twitter">Twitter! Social Network!</a></li>
+		</ul>	
+        </div>
+			
+	<div class="bfoot"></div>
+
+</div> <!-- end right //-->
+
+
+	<div id="left">
+		 
+	<div class="leftpost">
+
+	<div class="info">
+	
+	<div class="callender">
+		<div class="date">10/06</div>
+		<div class="month"><h3>2013</h3></div>
+	</div> <!-- end callender //-->
+
+		<p class="comments"><strong>Admin:</strong></p>
+
+		<p class="comments">R4-Owner</p>
+
+		<p class="extra"><strong>Posted in:</strong><br>
+		Home Page</p>
+		
+	
+	</div> <!-- end info //-->
+
+
+	</div> <!-- End Left Column -->
+
+	
+	<div class="rightpost">
+	<div class="cpost">
+		<h2><u>R4.Productionz</u></h2>
+		   
+		<p class="post">Welcome, If Your Seeing This Site Then You Are Also Electronically Acknowledging That You Have Read And Agree To The R4 Agreement (Terms Of Service), The R4 Activation Rules, And Anti Proxifier Rule!! Thankyou For Your Agreement, And Enjoy This All Educational Purposes Website! Havnt Seen The Agreement Before See It Here... <a href="index.html"><u>R4 Agreement(T.O.S)</u></a></p>
+		<p class="post">Reminder To All: R4 Works Monday To Friday Only!! That In NZ Standard Timezone!! So Dont Email Me In Weekend Timeframe, Expecting A Reply... I Wont Reply Until Monday!! Thanks For Your Understanding!!</p>
+<br>                
+</center>		   
+	</div>
+			  		 
+	</div> <!-- End Right Column -->	
+
+	<div class="leftpost">
+
+	<div class="info">
+	
+	<div class="callender">
+		<div class="date">20/06</div>
+		<div class="month"><h3>2013</h3></div>
+	</div> <!-- end callender //-->
+
+		<p class="comments"><strong>Admin:</strong></p>
+
+		<p class="comments">R4-Owner</p>
+
+		<p class="extra"><strong>Posted in:</strong><br>
+		Home Page</p>
+		
+	
+	</div> <!-- end info //-->
+
+	</div> <!-- End Left Column -->
+	
+	<div class="rightpost">
+	<div class="cpost">
+		<h2>Recent Good News</h2>
+<center>
+<h2>R4 IP Grab N Boot 1.1 Released (New Program)!</h2>
+<p class="post">R4 IP Grab N Boot Released, Special Skype IP Grabbing, Special Link Ip Grabbing, IM/PM Flood Stress Testing, Network Stress Testing Booter! <u><a href="pipg.html">Check It Out</a></u>  In Paidwares!</p>
+</center>
+<br>
+        
+
+		<p class="post"><a href="fsproxy.html"><img src="pics/sproxy.bmp" alt="S-Proxy 5.1" class="float-left" height="80" width="80"></a> 
+	      R4 S-Proxy Test N Grab 5.1 Is Released.... Now Fixed To Grab Http's And Test Sock5's, And Built In Range Maker! See Reade Me For All Details!
+	    </p>
+<br>
+		<p class="post"><a href="yjapan.html"><img src="images/favicon.ico" alt="R4 Free Y! Japan Package" class="float-left" height="80" width="80"></a> 
+	      New R4s Y! Japan ID Creator v1.0.. Now YOu Can Create Your Yahoo Japan Messenger Account's More Easy And Fast! Your Welcome Everyone.. Enjoy!
+	    </p>
+<br>
+<br>
+        
+	    <p class="post"><strong>R4 Live's On...<br></strong></p>
+        <blockquote>
+		<ul>
+			<li>New Paid Services, IP Grab N Boot, Check It Out!</li>
+			<li>New R4 Album Released! A True Love Story!</li>
+			<li>Goto Chat/Help Zone To Register A ID!</li>
+			<li>You Need A R4 Chat ID To Use Help!</li>
+			<li>You Need A R4 Chat ID To Use Private Softs!</li>
+		</ul>
+        </blockquote>
+
+	</div>
+			  		 
+	</div> <!-- End Right Column -->
+
+	
+	<div class="rss2"><center><a href="index.php"><span>R4 - Nothing Compares!! Thinking Outside The Box!!</span></a></center></div>
+  </div> <!-- end info //-->
+
+	</div> <!-- End Left Column -->
+	
+	
+
+
+ 
+<div id="footer">
+<center>
+<span class="smalltext">R4 Site Updated, Owned And Maintained By R4 Himself!</span>
+<br>
+<br>
+<br>
+<a href="http://www.dmca.com/Protection/Status.aspx?ID=c25920fc-163d-4775-9e6a-053146e0b45b" title="DMCA"> <img src ="http://images.dmca.com/Badges/dmca_copyright_protected150b.png?ID=c25920fc-163d-4775-9e6a-053146e0b45b"  alt="DMCA.com" /></a>
+<br>
+<a href="http://www.dmca.com/Protection/Status.aspx?ID=c25920fc-163d-4775-9e6a-053146e0b45b" title="DMCA"> <img src ="http://images.dmca.com/Badges/DMCA_logo-bw200w.png?ID=c25920fc-163d-4775-9e6a-053146e0b45b"  alt="DMCA.com" /></a>
+<br>
+<span>Copyright &copy; 2012 productionz.ws &bull; <a href="index.php">Design by R4</a></span>
+<br>
+<br>
+</center>
+</div>
+
+</body>
+</html>
